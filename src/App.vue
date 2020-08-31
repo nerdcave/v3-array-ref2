908f0613fd474f2c7188fa9497dbb6b217cdac84
Jay Elaraj <jay@nerdcave.com>
2020-08-31 13:42:10 -0400

    Adds ref array/primitive example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    itemsSorted: {{ itemsSorted }}
+    <br>
+    countPlus1: {{ countPlus1 }}
+    <hr/>
+    <br>
+    items: {{ items }}<br>
+    count: {{ count }}<br>
+    <button @click="items.push(Math.round(100*Math.random()))">add items</button>
+    <button @click="count++">inc count</button>
+    <br>
+    <p>itemsSorted is triggered, countPlus1 is not.</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */
+import { ref, toRef, reactive, computed } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  setup() {
+    const items = ref([1, 2, 3])
+    const count = ref(0)
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    const state = reactive({ items, count })
+
+    // does trigger itemsSorted computed but is this expected?
+    // I expect it should be toRef(state, 'items')
+    const itemsRef = ref(state.items)
+
+    // does not trigger computed (as expected), but toRef(state, 'count') does
+    const countRef = ref(state.count)
+    // const countRef = toRef(state, 'count')
+
+    const itemsSorted = computed(() => [...itemsRef.value].sort())
+    const countPlus1 = computed(() => countRef.value + 1)
+
+    return {items, count, itemsSorted, countPlus1}
+  }
+};
+</script>
